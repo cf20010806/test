2070e4729f2f4018e863bfa337b8bfaf8580b985
cf20010806 <3061326765@qq.com>
2021-04-21 18:49:05 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 0; i < 10; i++) {
+            for (int r = 0; r < i; r++) {
+                System.out.println("i*r"+"="+i*r);
+            }
+        }
+    }
+}
